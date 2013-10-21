207f4127f7d40cfdbfa9586da8cc2d8a49b16210
jaketrent <trent.jake@gmail.com>
2013-10-20 21:17:55 -0600

    regen dist

--- dist/angular-linkto.js
+++ dist/angular-linkto.js
@@ -1,1 +1,74 @@
-(function(){angular.module("angular-linkto",[]),angular.module("angular-linkto").service("keymaster",function(){return{replaceKey:function(a,b,c){return a.replace(":"+b,null!=c?c[b]:void 0)},findKeys:function(a){return a.match(/:[\w\d_\-]+/g)},replaceKeys:function(a,b){var c,d;(null!=b?"function"==typeof b.hasAttributes?b.hasAttributes():void 0:void 0)&&(b=b.toJSON()),d=this.findKeys(a);for(c in b)a=this.replaceKey(a,c,b);return a}}}),angular.module("angular-linkto").filter("linkTo",["keymaster",function(a){var b;return b=function(a){return a.match(/:.*\//)},function(c,d){if(null==c)throw new Error("Route required for linkTo()");if(c=a.replaceKeys(c,d),-1!==c.indexOf(":")){if(b(c))throw new Error("Must provide all data for named params in linkTo(), missing: "+a.findKeys(c)+", in route: "+c);c=c.substring(0,c.lastIndexOf("/"))}return c}}])}).call(this);
+(function() {
+  angular.module('angular-linkto', []);
+
+  angular.module('angular-linkto').service('keymaster', function() {
+    return {
+      replaceKey: function(str, key, params) {
+        return str.replace(":" + key, params != null ? params[key] : void 0);
+      },
+      findKeys: function(str) {
+        return str.match(/:[\w\d_\-]+/g);
+      },
+      replaceKeys: function(str, params) {
+        var key, keys;
+        if (params != null ? typeof params.hasAttributes === "function" ? params.hasAttributes() : void 0 : void 0) {
+          params = params.toJSON();
+        }
+        keys = this.findKeys(str);
+        for (key in params) {
+          str = this.replaceKey(str, key, params);
+        }
+        return str;
+      }
+    };
+  });
+
+  angular.module('angular-linkto').filter('linkTo', function(keymaster) {
+    var containsColon, containsColonAfterProtocol, containsColonBeforeSlash, containsProtocol, dropLastParam, throwUnfilledParamError;
+    containsColon = function(str) {
+      return str.indexOf(':') > -1;
+    };
+    containsColonAfterProtocol = function(str) {
+      return str.match(/^\w+:\/\/.*:/);
+    };
+    containsColonBeforeSlash = function(str) {
+      return str.match(/:[\w\d_\-]+\//);
+    };
+    containsProtocol = function(str) {
+      return str.match(/^\w+:\/\/.*/);
+    };
+    dropLastParam = function(str) {
+      return str.substring(0, str.lastIndexOf('/'));
+    };
+    throwUnfilledParamError = function(str) {
+      throw new Error("Must provide all data for named params in linkTo(), missing: " + (keymaster.findKeys(str)) + ", in route: " + str);
+    };
+    return function(route, params) {
+      if (route == null) {
+        throw new Error('Route required for linkTo()');
+      }
+      route = keymaster.replaceKeys(route, params);
+      if (containsProtocol(route)) {
+        if (containsColonAfterProtocol(route)) {
+          if (containsColonBeforeSlash(route)) {
+            throwUnfilledParamError(route);
+          } else {
+            route = dropLastParam(route);
+          }
+        }
+      } else if (containsColon(route)) {
+        if (containsColonBeforeSlash(route)) {
+          throwUnfilledParamError(route);
+        } else {
+          route = dropLastParam(route);
+        }
+      }
+      return route;
+    };
+  });
+
+}).call(this);
+
+/*
+//@ sourceMappingURL=angular-linkto.js.map
+*/
